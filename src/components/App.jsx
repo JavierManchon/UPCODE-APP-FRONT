4a328b18a38b5a1fd474fc86eba0fe4b49ff41c1
Mauzoom <mauro.quintana@bootcamp-upgrade.com>
2024-03-21 09:28:00 +0100

    feat : Payments + Figures + Loading components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import Catalogue from './pages/Catalogue/Catalogue'
+//import Catalogue from './pages/Catalogue/Catalogue'//
+import Payments from './pages/Payments/Payments'
 
 
 function App() {
   return (
     <div>
-      <Catalogue/>
+      <Payments/>
     </div>
   )
 }
---
+++ src/components/utils/Loading/Loading.jsx
@@ -0,0 +1,33 @@
+import React, { useRef, useEffect } from 'react';
+import './Loading.scss'; 
+
+function App() {
+  const titleRef = useRef(null);
+
+  async function animateTitle() {
+    titleRef.current.classList.add("animate");
+
+    const animations = titleRef.current.getAnimations();
+    await Promise.all(animations.map((animation) => animation.finished));
+
+    setTimeout(() => {
+      
+      titleRef.current.classList.remove("animate");
+      setTimeout(() => {
+        animateTitle();
+      }, 500);
+    }, 1000);
+  }
+
+  useEffect(() => {
+    animateTitle();
+  }, []); 
+
+  return (
+    <div className="App">
+      <h1 ref={titleRef}>&lt;UP_C0DE/&gt;</h1>
+    </div>
+  );
+}
+
+export default App;
