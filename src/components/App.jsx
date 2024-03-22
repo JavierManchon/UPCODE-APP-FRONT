88738033eebbc9f0061f3fcd50fcbd800f9727e2
rois90 <roissm90@gmail.com>
2024-03-22 07:59:56 +0100

    feat: asideTicket

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import Catalogue from './pages/Catalogue/Catalogue'
 import Header from './layout/Header/Header'
 import Footer from './layout/Footer/Footer'
+import AsideTickets from './pages/AsideTickets/AsideTickets';
+import ButtonComponent from './pages/Buttons/ButtonComponent';
 
 function App() {
   const [isLogged, setIsLogged] = useState(false);
@@ -9,6 +11,7 @@ function App() {
     <div>
       <Header isLogged={isLogged} setIsLogged={setIsLogged}/>
       <Catalogue/>
+      <AsideTickets setIsLogged={setIsLogged} isLogged={isLogged}/>
       <Footer/>
     </div>
   )
